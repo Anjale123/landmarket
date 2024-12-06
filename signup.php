843f7b15de11b903a6559ea82b719eb4a6667e41
Anjalee Kaushalya <139565339+Anjale123@users.noreply.github.com>
2024-12-06 10:53:20 +0000

    updated

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@
 ?>
 <?php
     if(isset($_POST["signup"])){
-        if($_POST["pwsd"] == $_POST["cpswd"]){
+        if($_POST["pswd"] == $_POST["cpswd"]){
             $fullname = mysqli_real_escape_string($connection,$_POST["fullname"]);
             $mail = mysqli_real_escape_string($connection,$_POST["mail"]);
 
@@ -11,23 +11,19 @@
             $result_set = mysqli_query($connection,$sql);
 
             if(isset($result_set)){
-                header("Location: Loginpage.php");
+                header("Location: login.php");
             }
         }
     }
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Signup Page</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom CSS -->
+     <link rel="stylesheet" href="bootstrap-5.3.3-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
@@ -36,27 +32,27 @@
     <div class="signup-form text-center p-5 shadow rounded">
         <img src="user.png" alt="">
         <h2 class="mb-4">Sign Up</h2>
-        <form>
-            <div class="form-group">
-                <label for="name"><b>Full Name</b></label>
+        <form method="post" action="signup.php">
+            <div class="form-group" style="margin-bottom: 10px">
+                <label for="name" ><b>Full Name</b></label>
                 <input type="text" name="fullname" class="form-control" style="margin-bottom: 25px;border-color:#06b30e;"id="name" placeholder="Enter your full name">
             </div>
             <div class="form-group">
-                <label for="email"><b>Email address</b></label>
+                <label for="email" ><b>Email address</b></label>
                 <input type="email" name="mail" class="form-control" style="margin-bottom: 25px;border-color:#06b30e;" id="email" placeholder="Enter your email">
             </div>
             <div class="form-group">
                 <label for="password"><b>Password</b></label>
-                <input type="password" name="pwsd" class="form-control" style="margin-bottom: 25px;border-color:#06b30e;" id="password" placeholder="Enter password">
+                <input type="password" name="pswd" class="form-control" style="margin-bottom: 25px;border-color:#06b30e;" id="password" placeholder="Enter password">
             </div>
             <div class="form-group">
-                <label for="confirm-password"><b>Confirm Password</b></label>
+                <label for="confirm-password" ><b>Confirm Password</b></label>
                 <input type="password" name="cpswd" class="form-control" style="margin-bottom: 25px;border-color:#06b30e;" id="confirm-password" placeholder="Confirm password">
             </div>
             <button type="submit" name="signup" class="btn btn-block" style="background-color: #06b30e; color:white;" style="margin-bottom: 10px">Sign Up</button>
         </form>
         <div class="mt-3">
-            Already have an account? <a href="Loginpage.php" style="color: #06b30e; text-decoration: none">Log in</a>
+            Already have an account? <a href="login.php" style="text-decoration: none;color:#06b30e ;">Log in</a>
         </div>
 
     </div>
@@ -65,6 +61,6 @@
 <!-- Bootstrap JS and dependencies -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="bootstrap-5.3.3-dist/js/bootstrap.min.js"></script>
 </body>
 </html>
