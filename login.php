9181822ed99ef786cb451e45e5217be5c8f9d083
Anjalee Kaushalya <139565339+Anjale123@users.noreply.github.com>
2024-12-19 07:25:09 +0000

    edit login page

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
         $pswd = mysqli_real_escape_string($connection,$_POST["pswd"]);
 
         if($mail != "" && $pswd != ""){
-            $sql1 = "SELECT * FROM users WHERE mail='{$mail}' AND pswd='{$pswd}'";
+            $sql1 = "SELECT * FROM users WHERE mail='{$mail}' AND pwsd='{$pswd}'";
 
             $result_set1 = mysqli_query($connection,$sql1);
 
